39806e07410429dcfc5651df8495934087e0f89d
jchans <jiangchenghans@gmail.com>
2020-12-09 17:41:04 +0800

    1209 practice

--- test/a20201207-03-form.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo json_encode($_POST, JSON_UNESCAPED_UNICODE);
--- test/a20201209-03-xlsx.php
+++ test/a20201209-03-xlsx.php
@@ -13,6 +13,7 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
 $sheet->setCellValue('A1', 'Hello World !');
+$sheet->setCellValue('A3', '老和簽到一下');
 
 $writer = new Xlsx($spreadsheet);
 $writer->save('hello world.xlsx');
