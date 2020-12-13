d61e0d75ca4817a548088013ba5fdb3eed8c7662
LaohanJ <jiangchenghans@gmail.com>
2020-12-14 00:39:15 +0800

    add comment to 02-cookie file

--- test/a20201210-02-cookie.php
+++ test/a20201210-02-cookie.php
@@ -1,4 +1,8 @@
 <?php
+// 上一個例子，第一次給 cookie 的時候，網頁會讀不到 cookie 的值
+// 用 $myc 變數的值來代替 cookie 的值被印出
+// 用一個 if 判斷式來確認 cookie 'mycookie' 的存在，如果不存在就給 $myc 一個值
+//
 if (isset($_COOKIE['mycookie'])) {
     $myc = $_COOKIE['mycookie'] + 1;
 } else {
