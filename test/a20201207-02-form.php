30dfa18501d11ae9a65d9650a3ce1381d0f15a88
LaohanJ <jiangchenghans@gmail.com>
2020-12-13 22:57:59 +0800

    add comment to form action

--- test/a20201207-02-form.php
+++ test/a20201207-02-form.php
@@ -7,6 +7,8 @@
 </head>
 <body>
 
+<!-- form 標籤裡的 action 設定，會在表格被 submit 後把表格送出的資料傳到指定的檔案 -->
+<!-- 參考： https://www.w3schools.com/tags/att_form_action.asp -->
 <form action="a20201207-03-form.php" name="form1" method="post">
     <input type="text" id="account" name="account" placeholder="帳號"><br>
     <input type="password" name="password" id="password" placeholder="密碼"><br>
