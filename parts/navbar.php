f59e92edbedd5d0779ede753a68717848ff90837
jchans <jiangchenghans@gmail.com>
2020-12-15 14:49:02 +0800

    modify navbar for login features

--- parts/navbar.php
+++ parts/navbar.php
@@ -12,32 +12,35 @@ if (!isset($pageName)) $pageName = '';
 </style>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="<?= WEB_ROOT ?>index_.php">Index</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item <?= $pageName == 'ab-list' ? active : '' ?>">
-                <a class="nav-link" href="<?= WEB_ROOT ?>ab-list.php">通訊錄列表</a>
-            </li>
-            <li class="nav-item <?= $pageName == 'ab-insert' ? active : '' ?>">
-                <a class="nav-link" href="<?= WEB_ROOT ?>ab-insert.php">新增通訊錄</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
+    <div class="container">
+        <a class="navbar-brand" href="<?= WEB_ROOT ?>index_.php">Index</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item <?= $pageName == 'ab-list' ? active : '' ?>">
+                    <a class="nav-link" href="<?= WEB_ROOT ?>ab-list.php">通訊錄列表</a>
+                </li>
+                <li class="nav-item <?= $pageName == 'ab-insert' ? active : '' ?>">
+                    <a class="nav-link" href="<?= WEB_ROOT ?>ab-insert.php">新增通訊錄</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <?php if (isset($_SESSION['admin'])) : ?>
+                    <li class="nav-item <?= $pageName == 'ab-list' ? active : '' ?>">
+                        <a class="nav-link"><?= $_SESSION['admin']['account'] ?></a>
+                    </li>
+                    <li class="nav-item>
+                    <a class=" nav-link" href="<?= WEB_ROOT ?>logout.php">登出</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item <?= $pageName == 'login' ? active : '' ?>">
+                        <a class="nav-link" href="<?= WEB_ROOT ?>login.php">登入</a>
+                    </li>
+                <?php endif; ?>
+
+            </ul>
+        </div>
     </div>
 </nav>
