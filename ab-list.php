6706e054a4bb0cbc19a6b7800f6606e3e3c92b04
jchans <jiangchenghans@gmail.com>
2020-12-16 11:01:28 +0800

    class notes

--- ab-list.php
+++ ab-list.php
@@ -76,6 +76,12 @@ $row2 = $stmt->fetchAll();
                 </ul>
             </nav>
         </div>
+        <div class="col d-flex flex-row-reverse bd-highlight">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
     </div>
 
     <table class="table table-striped table-bordered">
