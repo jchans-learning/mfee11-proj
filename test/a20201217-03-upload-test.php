c6dfcb94f360acd7769c0f77f546735e88f2786e
jchans <jiangchenghans@gmail.com>
2020-12-17 11:42:28 +0800

    add uuid ref in comment

--- test/a20201217-03-upload-test.php
+++ test/a20201217-03-upload-test.php
@@ -3,6 +3,12 @@ $output = [];
 
 $upload_folder = __DIR__. '/../uploads';
 
+$ext_map = [
+  'image/jpeg' => '.jpg',
+  'image/png' => '.png',
+    'image/gif' => '.gif',
+];
+
 if(!empty($_FILES)){
     $output['file'] = $_FILES;
 
@@ -18,4 +24,11 @@ echo json_encode($output, JSON_UNESCAPED_UNICODE);
 //
 // 要搬動檔案只有 move_uploaded_file() 可以用
 // 參考資料： https://www.php.net/manual/en/function.move-uploaded-file.php
+//
+// uniqid()
+// 用時間（？）產生獨特的 id
+// 參考資料： https://www.php.net/manual/en/function.uniqid.php
+//
+// uuid 產生參考：
+// https://stackoverflow.com/questions/2040240/php-function-to-generate-v4-uuid
 
