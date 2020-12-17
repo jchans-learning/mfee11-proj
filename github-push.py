f9887221c2c7c928ad4c20bfa238aee794655d68
jchans <jiangchenghans@gmail.com>
2020-12-17 10:04:14 +0800

    modify comment

--- github-push.py
+++ github-push.py
@@ -7,7 +7,4 @@ commitCommand = 'git commit -m ' + '"' + commitMessage + '"'
 os.system(commitCommand)
 os.system('git push')
 
-# macbook 目前有些版本可能預設還是 python 2 ，有裝 python3
-# 可以用以下指令安裝，執行 venv
-# python3 -m venv tutorial-env
-# source tutorial-venv/bin/activate
+# macbook 目前有些版本可能預設還是 python 2 ，有裝 python3 ，用 python3 執行
