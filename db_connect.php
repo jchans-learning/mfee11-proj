bc84fd636da8a65ce4dd267d28429523d18d0d63
jchans <jiangchenghans@gmail.com>
2020-12-18 10:15:05 +0800

    add notes to comment

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,5 @@
 <?php
 
-// 以下註解掉的部分存在另一個檔案裡
-/*
-$db_host = 'localhost';
-$db_name = 'mfee11c';  // 資料庫名稱
-$db_user = 'root'; // 資料庫要設定別的帳號
-$db_pass = '';
- */
-
 include 'db_key.php';
 
 $dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8";
@@ -18,8 +10,28 @@ $pdo_options = [
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
 ];
 
+// 這行用 PDO 連結資料庫
 $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
 
 if (!isset($_SESSION)) {
     session_start();
 }
+
+// 筆記
+//
+// 這個檔案的目的是連結伺服器。利用 PHP 的 PDO 功能。
+// 參考資料：
+// 關於上面 $pdo_options 用到的東西：
+// https://www.php.net/manual/en/pdo.constants.php
+// https://www.php.net/manual/en/ref.pdo-mysql.php
+//
+// PHP Data Objects (PDO) 簡易使用說明(1)：
+// https://pjchender.blogspot.com/2015/08/php-data-objects-pdo.html
+//
+// 以下註解掉的部分存在另一個檔案裡，用 include 的方式去引用，並且放入 .gitignore
+/*
+$db_host = 'localhost';
+$db_name = '資料庫名稱';
+$db_user = '資料庫要設定別的帳號';
+$db_pass = '登入資料庫用的密碼';
+ */
